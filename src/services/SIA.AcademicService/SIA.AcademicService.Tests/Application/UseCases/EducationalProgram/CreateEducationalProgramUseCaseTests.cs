db6d2996affdec08f81bbc603995794baadf2f7a
Gilberto Villa <gilvillar@hotmail.com>
2026-08-19 20:06:47 -0600

    ACAD-14  Implementar pruebas iniciales de AcademicService (#46)
    Se implementa la estructura basica para la ejecución de pruebas en el servicio de AcademicService

---
+++ src/services/SIA.AcademicService/SIA.AcademicService.Tests/Application/UseCases/EducationalProgram/CreateEducationalProgramUseCaseTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIA.AcademicService.Tests.Application.UseCases.EducationalProgram
+{
+    internal class CreateEducationalProgramUseCaseTests
+    {
+    }
+}
